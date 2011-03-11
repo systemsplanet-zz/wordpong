863de09787228668934b85d6fd69eae017ca738f
Mike Lawrence <mike@systemsplanet.com>
2011-03-10 23:16:00 -0500

    fix objectify jar

---
+++ war/WEB-INF/lib/0clean.cmd
@@ -0,0 +1,11 @@
+rem remove unused jars
+del appengine-api-1.0-sdk-1.4.2.jar
+del appengine-api-labs-1.4.2.jar
+del appengine-jsr107cache-1.4.2.jar
+del datanucleus-appengine-1.0.8.final.jar
+del datanucleus-core-1.1.5.jar
+del datanucleus-jpa-1.1.5.jar
+del geronimo-jpa_3.0_spec-1.1.1.jar
+del geronimo-jta_1.1_spec-1.1.1.jar
+del jdo2-api-2.3-eb.jar
+del jsr107cache-1.1.jar
