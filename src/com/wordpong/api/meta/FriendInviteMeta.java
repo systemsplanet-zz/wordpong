8b919330469941ecfc0388b113408a75d0c947e6
Mike Lawrence <mike.lawrence@theice.com>
2011-04-04 16:51:41 -0400

    configured eclipse

--- src/com/wordpong/api/meta/FriendInviteMeta.java
+++ src/com/wordpong/api/meta/FriendInviteMeta.java
@@ -1,6 +1,6 @@
 package com.wordpong.api.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-04-03 22:33:11")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-04-04 16:48:50")
 /** */
 public final class FriendInviteMeta extends org.slim3.datastore.ModelMeta<com.wordpong.api.model.FriendInvite> {
 
