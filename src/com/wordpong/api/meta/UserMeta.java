8b154dd76a70a6f103d3d2594bb292379d2f7655
Mike Lawrence <mike@systemsplanet.com>
2011-04-04 22:41:20 -0400

    revert theaded model

--- src/com/wordpong/api/meta/UserMeta.java
+++ src/com/wordpong/api/meta/UserMeta.java
@@ -1,6 +1,6 @@
 package com.wordpong.api.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-04-04 16:48:50")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-04-04 18:27:53")
 /** */
 public final class UserMeta extends org.slim3.datastore.ModelMeta<com.wordpong.api.model.User> {
 
