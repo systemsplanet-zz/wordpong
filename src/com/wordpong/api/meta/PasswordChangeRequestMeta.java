e7aabc0f5c78f95e8954831d4ae18a0f24d10222
Mike Lawrence <mike@systemsplanet.com>
2011-04-11 22:29:16 -0400

    fix class path

--- src/com/wordpong/api/meta/PasswordChangeRequestMeta.java
+++ src/com/wordpong/api/meta/PasswordChangeRequestMeta.java
@@ -1,6 +1,6 @@
 package com.wordpong.api.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-04-11 22:16:01")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-04-11 22:23:02")
 /** */
 public final class PasswordChangeRequestMeta extends org.slim3.datastore.ModelMeta<com.wordpong.api.model.PasswordChangeRequest> {
 
