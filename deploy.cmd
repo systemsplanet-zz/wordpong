28b61b4cb664dd881918dfddd9ec0f39407cf3cf
unknown <mlawrence@powerpro.(none)>
2012-03-08 22:03:25 -0500

    upgrade to App engine 1.6.3

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
 set GAESDK=C:\eclipse\plugins\com.google.appengine.eclipse.sdkbundle_1.5.0.r36v201105092302\appengine-java-sdk-1.5.0
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_25
+set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.6.0_21
 "%JAVA_HOME%\bin\java" -cp "%GAESDK%\lib\appengine-tools-api.jar" com.google.appengine.tools.admin.AppCfg update war
 pause
--- war/WEB-INF/lib/0clean.cmd
+++ war/WEB-INF/lib/0clean.cmd
@@ -3,11 +3,11 @@ rem del appengine-api-1.0-sdk-1.5.0.jar
 rem del appengine-api-labs-1.5.0.jar
 rem del appengine-jsr107cache-1.5.0.jar 
 
-del appengine-jsr107cache-1.5.0.jar
-del datanucleus-appengine-1.0.8.final.jar
-del datanucleus-core-1.1.5.jar
-del datanucleus-jpa-1.1.5.jar
-del geronimo-jpa_3.0_spec-1.1.1.jar
-del geronimo-jta_1.1_spec-1.1.1.jar
-del jdo2-api-2.3-eb.jar
-del jsr107cache-1.1.jar
+del jsr107cache-1.1.jar 
+del datanucleus-appengine-1.0.10.final.jar 
+del datanucleus-core-1.1.5.jar 
+del datanucleus-jpa-1.1.5.jar 
+del geronimo-jpa_3.0_spec-1.1.1.jar 
+del geronimo-jta_1.1_spec-1.1.1.jar 
+del jdo2-api-2.3-eb.jar 
+del appengine-jsr107cache-1.6.3.jar 
